3a9d377068da633defeee52f5e5f29efc38a14ab
Marianna <marbar2009tyumen@gmail.com>
2021-09-15 20:17:18 +0300

    comment to test

--- calculator.py
+++ calculator.py
@@ -1,17 +1,17 @@
 # homework 1
-a = int(input())
-operator = input()
-b = int(input())
+a = int(input("Введите первое число:"))
+operator = input("Введите знак операции:")
+b = int(input("Введите второе число:"))
 if operator == "+":
-    print(a+b)
+    print("Результат: ", a+b)
 elif operator == "-":
-    print(a-b)
+    print("Результат: ", a-b)
 elif operator == "*":
-    print(a*b)
+    print("Результат: ", a*b)
 elif operator == "/":
     if b == 0:
         print("Universe error: do not divide by zero!")
     else:
-        print(a/b)
+        print("Результат: ", a/b)
 else:
     print("Oops, your math is too high for this script=)")
